9c720f78fef9ad4bb9501c9b9e5267567014df99
NickJ202 <50843357+NickJ202@users.noreply.github.com>
2021-05-24 10:30:02 -0400

    Update settings.py

--- saleor/settings.py
+++ saleor/settings.py
@@ -68,7 +68,7 @@ INTERNAL_IPS = get_list(os.environ.get("INTERNAL_IPS", "127.0.0.1"))
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get('PG_DATABASE_URL'), conn_max_age=600
+        default=(os.environ.get('PG_DATABASE_URL') if os.environ.get('PG_DATABASE_URL') else "postgres://saleor:saleor@localhost:5432/saleor"), conn_max_age=600
     )
 }
 
