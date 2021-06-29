937d5eba844901f44da9585ba068f331b1cacd1c
VinceJuliano <VinceJuliano>
2021-06-29 15:51:01 -0400

    remove maillist migration

--- saleor/maillist/models.py
+++ saleor/maillist/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class MailListParticipant(models.Model):
     email = models.EmailField(unique=True)
     created_date = models.DateTimeField(default=timezone.now, editable=False)
