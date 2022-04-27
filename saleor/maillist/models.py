ce55eda0c458d6802d66d69270be7914490a9e32
NickJ202 <50843357+NickJ202@users.noreply.github.com>
2022-04-27 11:02:04 -0400

    Update models.py

--- saleor/maillist/models.py
+++ saleor/maillist/models.py
@@ -1,8 +1,11 @@
 
 from django.db import models
 from django.utils import timezone
-
-
+    
 class MailListParticipant(models.Model):
     email = models.EmailField(unique=True)
+    first_name = models.TextField()
+    last_name = models.TextField()
+    state = models.TextField()
+    active = models.BooleanField()
     created_date = models.DateTimeField(default=timezone.now, editable=False)
